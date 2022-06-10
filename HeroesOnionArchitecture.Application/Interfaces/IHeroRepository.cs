89302e7e705fddd48c6a832f536715979123382c
Jean Carlos Reyes Encarnacion <yacalo45@hotmail.es>
2022-06-10 01:01:39 -0400

    Services Registration Work

--- HeroesOnionArchitecture.Application/Interfaces/IHeroRepository.cs
+++ HeroesOnionArchitecture.Application/Interfaces/IHeroRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HeroesOnionArchitecture.Core.Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,10 @@ namespace HeroesOnionArchitecture.Core.Application.Interfaces
 {
     public interface IHeroRepository
     {
-            }
+        Task AddAsync(Hero hero);
+        Task<List<Hero>> GetAllAsync(Hero hero);
+        Task<Hero> GetByIdAsync(int id);
+        Task UpdateAsync(Hero hero);
+        Task DeleteAsync(Hero hero);
+    }
 }
---
+++ HeroesOnionArchitecture.Persistence/ServicesRegistration.cs
@@ -0,0 +1,21 @@
+﻿using HeroesOnionArchitecture.Infrastructure.Persistence.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeroesOnionArchitecture.Infrastructure.Persistence
+{
+    public static class ServicesRegistration
+    {
+        public static void AddPersistanceInfrastructure(this IServiceCollection service, IConfiguration configuration) {
+
+            service.AddDbContext<ApplicationContext>(options=>options.UseSqlServer(configuration.GetConnectionString("MyConnectionString")));
+        
+        }
+    }
+}
